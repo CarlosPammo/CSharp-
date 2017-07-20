4cdc5857c3ce1588af2cb1e4b93cab2f2f6831fa
RichardEcos <richard_ecos@hotmail.com>
2017-07-20 10:48:29 -0400

    Add an example about simple inheritance

---
+++ Classes/ClassesExercises/ClassesExercises/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassesExercises
+{
+    internal class Product
+    {
+        /// <summary>
+        /// Gets or sets the name of the product. 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the brand of the product. 
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Constructor for a product.
+        /// </summary>
+        /// <param name="name">Name for a product</param>
+        /// <param name="brand">Brand for a product</param>
+        public Product(string name, string brand)
+        {
+            Name = name;
+            Brand = brand;
+        }
+        
+        protected virtual float GetDiscount()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "Product: " + Name + " " + Brand + " - discount: " + GetDiscount();
+        }
+    }
+}
---
+++ Classes/ClassesExercises/ClassesExercises/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product zucaritas = new Product("Zucaritas", "Kellogs");
+            Shampoo shampoo = new Shampoo("Ceramidas", "Sedal");
+
+            Console.WriteLine(zucaritas);
+            Console.WriteLine(shampoo);
+        }
+    }
+}
