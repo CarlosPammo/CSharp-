b6d5ccb316ac0d1b99840b82482b9bcf578c3a5d
RichardEcos <richard_ecos@hotmail.com>
2017-07-25 12:22:37 -0400

    Add example for Exceptions

---
+++ Classes/ClassesExercises/Exceptions/NumberException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class NumberException: Exception
+    {
+        public NumberException(): base("Number error")
+        {
+            LogNumberException();
+        }
+        public NumberException(string str)
+        {
+            Console.WriteLine("{0} Is not a number");
+        }
+        public NumberException(Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+
+        public static void LogNumberException()
+        {
+            Console.WriteLine("[Number exception] - The value is negative");   
+        }
+    }
+}
---
+++ Classes/ClassesExercises/Exceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        public static bool ValidateNumber(string number)
+        {
+            var i = double.Parse(number);
+            if (i < 0)
+            {
+                NumberException.LogNumberException();
+                return false;
+            }
+
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            var valid = false;
+            while (!valid)
+            {
+                Console.Clear();
+                Console.Write("Insert a number: ");
+                var s = Console.ReadLine();
+
+                try
+                {
+                    valid = ValidateNumber(s);
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (NumberException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                catch (Exception ex)
+                {
+                    throw new NumberException(ex);
+                }
+                finally
+                {
+                    Console.WriteLine("Program completed");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
