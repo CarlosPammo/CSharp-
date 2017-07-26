c790f666559a1aee53520e3b33299be7fe375088
RichardEcos <richard_ecos@hotmail.com>
2017-07-26 11:53:09 -0400

    Add example to linq

---
+++ Classes/ClassesExercises/Linq/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Database
+    {
+        public static List<Product> Products()
+        {
+            return new List<Product>
+                {
+                    new Product {Id = 1, Name = "Van camps", Amount = 10},
+                    new Product {Id = 2, Name  = "Nutella", Amount = 16},
+                    new Product {Id = 3, Name  = "Nesquick", Amount = 17}
+                };
+        }
+
+        public static List<Sales> Sales()
+        {
+            return new List<Sales>
+                {
+                    new Sales {Id = 1, ProductId = 1, TotalPrice = 10, QuantitySold = 3},
+                    new Sales {Id = 3, ProductId  = 3, TotalPrice = 17, QuantitySold = 7}
+                };
+        }
+    }
+}
---
+++ Classes/ClassesExercises/Linq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = Database.Products();
+            var sales = Database.Sales();
+            var updatedStockProduct = (from product in products
+                                       join quantity in sales on product.Id equals quantity.ProductId
+                select new
+                {
+                    Product = product.Name,
+                    Stock = product.Amount - quantity.QuantitySold
+                });
+
+            foreach (var product in updatedStockProduct)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
